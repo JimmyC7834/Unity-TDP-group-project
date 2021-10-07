c21199c44edee354eac32893cf969254816e81f4
JimmyC7834 <42904673+JimmyC7834@users.noreply.github.com>
2021-10-06 23:29:43 -0700

    Update TimerTrigger.cs

--- TDP/Assets/Scripts/TimerTrigger.cs
+++ TDP/Assets/Scripts/TimerTrigger.cs
@@ -5,18 +5,19 @@ using UnityEngine.Events;
 public class TimerTrigger : MonoBehaviour
 {
     [SerializeField] private float timeInterval = default;
-    [SerializeField] private bool awakeStart = default;
-    private bool awaked = false;
+    // [SerializeField] private bool awakeStart = default;
+    // private bool awaked = false;
     public UnityEvent OnEarlyUpdate;
     public UnityEvent OnLateUpdate;
 
     private void OnEnable()
     {
-        if (!awaked && !awakeStart)
-        {
-            awaked = true;
-            return;
-        }
+        // if (!awaked && !awakeStart)
+        // {
+        //     Debug.Log("awake");
+        //     awaked = true;
+        //     return;
+        // }
 
         StartCoroutine(Timer(timeInterval));
     }
