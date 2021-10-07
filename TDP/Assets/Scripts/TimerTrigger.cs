1f6208742fad5d2d6ae97718f8d1a75744744112
JimmyC7834 <42904673+JimmyC7834@users.noreply.github.com>
2021-10-06 22:30:13 -0700

    added shake animations

--- TDP/Assets/Scripts/TimerTrigger.cs
+++ TDP/Assets/Scripts/TimerTrigger.cs
@@ -5,11 +5,19 @@ using UnityEngine.Events;
 public class TimerTrigger : MonoBehaviour
 {
     [SerializeField] private float timeInterval = default;
+    [SerializeField] private bool awakeStart = default;
+    private bool awaked = false;
     public UnityEvent OnEarlyUpdate;
     public UnityEvent OnLateUpdate;
 
     private void OnEnable()
     {
+        if (!awaked && !awakeStart)
+        {
+            awaked = true;
+            return;
+        }
+
         StartCoroutine(Timer(timeInterval));
     }
 
